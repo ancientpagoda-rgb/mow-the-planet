62cff0d9f4863105d6ec051e9ced89a9d93bcecb
ancientpagoda-rgb <ancientpagoda@gmail.com>
2026-08-16 20:13:55 -0500

    Add camera controller entrypoint

---
+++ game-entry.js
@@ -0,0 +1,3 @@
+// Load input/camera enhancements before the simulation creates OrbitControls.
+await import("./planet-keyboard-camera.js?v=camera-controller2");
+await import("./game-loader.js?v=castle-visible2-resources1-stones1-autostart2");
